d6834b29fc3e50d440aae78535f1034f604c001a
Rana-rene <rene.trujillo01@student.csulb.edu>
2022-09-12 22:12:06 -0700

    Update

--- js/ex1.js
+++ js/ex1.js
@@ -1,7 +1,7 @@
 const input = prompt("Enter a number from 1-100:")
-if (input = num ) {
-    console.log(`${input} is not a number number1`);
+if (input != num ) {
+    alert(`${input} is not a number number1`);
 }
 else {
-    console.log(`${input} is a number`);
+    alert(`${input} is a number`);
 }
